04a638dee547bcd4f66ca9a8d0c68289cd26bd6f
Ramger Deivi Duran Cortorreal <rmg-51@outlook.com>
2024-07-04 20:46:27 -0600

    Setup y el extraible del index

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Descomprime el archivo dragond_find.tar.gz
+tar -xzvf ./dragon_find.tar.gz 
+
+# Ejecuta el script de requisitos desde el directorio requeriments
+sudo sh ./requeriments/requeriments.sh
+
+
+exit
+
+
