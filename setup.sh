9cdc26ff979f1aa554b03f68dff823748841343b
isntzero <rmg-51@outlook.com>
2024-07-09 00:36:15 -0600

    Fix menu acsii

--- setup.sh
+++ setup.sh
@@ -1,56 +1,47 @@
 #!/bin/bash
 
-#menu acsii
+# Menú ASCII
 
-cowsay -f dragon "Hola bievenido a dragon_find" | lolcat
+cowsay -f dragon "Hola, bienvenido a dragon_find" | lolcat
 echo "Para continuar debes elegir una opción" | lolcat
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" 
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "[1] Instalar requisitos" | lolcat
 echo "[2] Ejecutar script" | lolcat
 echo "[3] Salir" | lolcat
 read execute
 
-if [$execute=="1"]; then 
+if [ "$execute" == "1" ]; then 
+    clear
+    echo "Instalando requisitos" | lolcat
     # Descomprime el archivo dragond_find.tar.gz
     tar -xzvf ./dragon_find.tar.gz 
     # Ejecuta el script de requisitos desde el directorio requeriments
     sudo sh ./requeriments/requeriments.sh 
-    # Volver a ejecutar menu
+    # Volver a ejecutar menú
     sudo sh ./dragon_find/setup.sh
 
-elif [$execute=="2"]; then
-    # Ejecuta el script de requisitos desde el directorio requeriments
+elif [ "$execute" == "2" ]; then
+    clear
+    # Ejecuta el script principal
     sudo sh ./dragon_find/dragonfind.sh
     
-elif [$execute=="3"]; then
+elif [ "$execute" == "3" ]; then
+    clear
     # Salir del script
     exit
 
 else
-    # Si no se selecciona ninguna opción, volver a ejecutar el menu
+    clear
+    # Si no se selecciona ninguna opción válida, volver a ejecutar el menú
     cowsay -f dragon "Ha ocurrido un error" | lolcat
-    echo "Quiero continuar [y/n]" | lolcat
+    echo "¿Quieres continuar? [y/n]" | lolcat
     read continue
-    if [$continue=="y"]; then
+    if [ "$continue" == "y" ]; then
         sudo sh ./dragon_find/setup.sh
-        elif [$continue=="n"]; then
+    elif [ "$continue" == "n" ]; then
         exit
-        else
+    else
         cowsay -f dragon "Ha ocurrido un error" | lolcat
         sudo sh ./dragon_find/setup.sh
-        fi
+    fi
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
