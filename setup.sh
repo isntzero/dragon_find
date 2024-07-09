04e3f58e0d447b955014a4fe7c9a5bab7a51128e
isntzero <rmg-51@outlook.com>
2024-07-09 01:01:18 -0600

    fix sleep 5

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ tar -xzvf ./dragon_find.tar.gz
 echo "Archivo descomprimido correctamente." | lolcat
 clear
 cowsay -f dragon "Gracias por instalarme, no olvides de hacer tu ls para comprobar que el directorio se creo" | lolcat
-sleep 10
+sleep 5
 exit   
